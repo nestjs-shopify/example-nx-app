b0b46eb30c756907fa5a6b91e62aa84abad17b7a
llong2195 <nduylong9501@gmail.com>
2023-04-19 12:56:56 +0700

    add migration

---
+++ data-source.ts
@@ -0,0 +1,29 @@
+import 'reflect-metadata';
+import { config } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+import { ConfigService } from '@nestjs/config';
+
+config();
+const configService = new ConfigService();
+
+export const AppDataSource = new DataSource({
+  type: (configService.get<string>('DATABASE_CONNECTION') as any) || 'mysql',
+  host: configService.get<string>('DATABASE_HOST'),
+  port: configService.get<number>('DATABASE_PORT'),
+  username: configService.get<string>('DATABASE_USERNAME'),
+  password: configService.get<string>('DATABASE_PASSWORD'),
+  database: configService.get<string>('DATABASE_DB_NAME'),
+  dropSchema: false,
+  keepConnectionAlive: true,
+  logging: 'all',
+  logger: 'advanced-console',
+  migrations: ['./apps/api/src/app/database/migrations/*.ts'],
+  entities: [],
+  // ssl: {
+  //     require: false,
+  //     rejectUnauthorized: false,
+  // },
+} as DataSourceOptions);
+
+console.log(AppDataSource.options);
