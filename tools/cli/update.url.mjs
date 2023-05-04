61efb56d6f286fccfe26fac60082fdfdcea6ffc0
llong2195 <nduylong9501@gmail.com>
2023-05-04 13:55:30 +0700

    change spawn to exec and add description

--- tools/cli/update.url.mjs
+++ tools/cli/update.url.mjs
@@ -3,6 +3,7 @@
 import { exec } from 'child_process';
 import { config } from 'dotenv';
 import updateDotenv from 'update-dotenv';
+import chalk from 'chalk';
 
 async function updateEnv() {
   if (process.argv.length > 2) {
@@ -14,18 +15,14 @@ async function updateEnv() {
   }
 }
 
-async function updateUrl() {
-  await new Promise((resolve) => {
-    const apiKey = process.env.SHOPIFY_API_KEY;
-    const appUrl = process.env.HOST;
-    const redirectUrls = `${appUrl}/api/online/auth,${appUrl}/api/online/callback,${appUrl}/api/offline/auth,${appUrl}/api/offline/callback`;
-
+async function updateUrl(apiKey, appUrl, redirectUrls) {
+  await new Promise((resolve, reject) => {
     const updateCmd = exec(`npm run shopify app update-url -- --api-key=${apiKey} --app-url=${appUrl} --redirect-urls=${redirectUrls}`);
     updateCmd.stdout.on('data', (data) => {
       console.log(`${data}`);
     });
-    updateCmd.stderr.on('data', (data) => {
-      console.error(`${data}`);
+    updateCmd.stderr.on('data', (err) => {
+      reject(err)
     });
     updateCmd.on('close', (code) => {
       resolve(code);
@@ -38,5 +35,16 @@ async function updateUrl() {
 
   config();
 
-  await updateUrl();
+  const apiKey = process.env.SHOPIFY_API_KEY;
+  const appUrl = process.env.HOST;
+  const redirectUrls = `${appUrl}/api/online/auth,${appUrl}/api/online/callback,${appUrl}/api/offline/auth,${appUrl}/api/offline/callback`;
+
+  try {
+    await updateUrl(apiKey, appUrl, redirectUrls);
+  } catch (error) {
+    const updateCmd = `npm run shopify app update-url -- --api-key=${apiKey} --app-url=${appUrl} --redirect-urls=${redirectUrls}`
+    console.log(chalk.red('You haven\'t logged in to Shopify CLI!'))
+    console.log(chalk.yellow(`\nplease run command:\n`))
+    console.log(chalk.green(`${updateCmd}\n\n`))
+  }
 })();
